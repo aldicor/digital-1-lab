eba257636b34601a2df8b11acb30c88d57eec470
aldicor <aldiazco@unal.edu.co>
2024-12-11 21:24:54 -0500

    Implementacion en FPGA

--- lab2/nivelcarga.v
+++ lab2/nivelcarga.v
@@ -1,18 +1,25 @@
-`include "restador.v"
+//`include "restador.v"
+//`include "descarga.v"
+//`include "sumador.v"
 
-module nivelcarga(A,B,green,yellow,critical);
+module nivelcarga(A,B,green,yellow,critical,descarga1,descarga2);
     input [3:0] A,B;
-    output green, yellow, critical;
+    output green, yellow, critical,descarga1,descarga2;
     wire [3:0] suma;
     wire co_s, co_r;
     wire in;
-    assign in = 1'b1;
+    assign in = 1'b0;
+    wire des1,des2;
 
-    adder4b    sumaAB(A,B,co_s, suma);
+    adder4b  sumaAB(~A,~B,co_s, suma);
     restador resta(suma,co_r);
+    bateria2 alertas(~A,~B,des1,des2);
+    
+    assign descarga1 = ~des1;
+    assign descarga2 = ~des2;
 
-    assign green    = in&co_s;
-    assign yellow   = in&~co_s&co_r;
-    assign critical = in&~co_s&~co_r; 
+    assign green    = ~(co_s);
+    assign yellow   = ~(~co_s&co_r);
+    assign critical = ~(~co_s&~co_r); 
 
 endmodule
