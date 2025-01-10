3c445423caeafb34697efbe88a734c78683dc589
aldicor <aldiazco@unal.edu.co>
2025-01-10 13:08:47 -0500

    modulo de divisor de frecuencia junto con señal para seleccionar segmento y unidad

---
+++ lab3/clockdiv.v
@@ -0,0 +1,18 @@
+module clockdiv #(parameter div = 16'd50000)(clin, clout);
+    input clin;
+    output reg clout;
+
+    reg [15:0] count = 16'd0;
+
+    initial begin
+        clout <= 0;
+    end
+    always @(posedge clin) begin
+        count <= count + 16'd1;
+        if (count == div-1) begin
+            count <= 16'd0;
+            clout <= ~clout;
+        end
+    end
+
+endmodule
