f27825743c7c198c2a0c1e0766713515f2f11609
aldicor <aldiazco@unal.edu.co>
2025-01-12 12:44:34 -0500

    primera version de semaforo sencillo - tiempos de estados contanstes

---
+++ fproject/quartus/freq_div.v
@@ -0,0 +1,20 @@
+module clockdiv #(parameter n = 50000000) (clkin, clkout);
+    input   clkin;
+    output reg clkout;
+
+    reg [25:0] count = 0;
+
+    initial begin
+        clkout = 0;
+    end
+
+    always @(posedge clkin) begin
+        count <= count +1;
+        if (count == n-1) begin
+            count <=0;
+            clkout = ~clkout;
+        end
+    end
+
+
+endmodule
---
+++ fproject/quartus/output_files/traffic_control.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 23.1std.1 Build 993 05/14/2024 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE10E22) Path("/home/aldico/Documents/digital-1-lab/fproject/quartus/output_files/") File("traffic_control.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
